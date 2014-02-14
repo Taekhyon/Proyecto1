e803c337cc410f809141cd5e198edb557bdd5a56
Jose Octavio Esparza <jose.esparza@live.com.xm>
2014-02-14 11:34:25 -0600

    Proyecto1
    Es el proyecto para funciones básicas de dispositivos.

---
+++ js/acciones.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(){
+	document.addEventListener("deviceready",
+	  function(){
+	  $('#B1').tap(function(){
+		  navigator.notificatio,beep(1);
+		  
+		  });	//tap B1				
+	});  //device ready
+
+});  //Ready
